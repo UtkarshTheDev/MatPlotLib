cfd6b654d42843868a742beece5f15d2bcd2fff2
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-08-22 13:07:15 +0530

    feat: Improved DataFrame Usage

--- dataframeUsage.py
+++ dataframeUsage.py
@@ -6,5 +6,7 @@ plt.ylabel("Test")
 plt.title("ODI vs Test")
 
 
-plt.plot(df.ODI, df.Test)
+plt.plot(df.ODI,color="red", marker="o", markerfacecolor="blue", markersize=7, ls="--", linewidth=3)
+plt.plot(df.Test, color="blue", marker="d", markerfacecolor="green", markersize=7, ls="-", linewidth=3)
+plt.legend(loc="upper right")
 plt.show()
